d22285611606a52169b21c5869a90190689e02d4
Bagas Alan <Bagassialan14@gmail.com>
2026-09-25 22:18:33 +0700

    fix controller namespace

--- src/app/Http/Controllers/Api/V1/LaporanController.php
+++ src/app/Http/Controllers/Api/V1/LaporanController.php
@@ -1,5 +1,8 @@
 <?php
 declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\V1;
+use App\Http\Controllers\Controller;
 use App\Services\LayananLaporan;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
